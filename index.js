ce9666d0f02b6d850ed1e66e7a2916cb1e7c6b36
JortvD <jortvandriel@hotmail.com>
2017-11-02 20:15:04 +0100

    Added the index.js file

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = function() {
+	var self = {};
+
+	var package = require("./package.json");
+	
+	self.name = package["name"];
+	self.version = package["version"];
+	self.dependencies = [
+		"cerus-server",
+		"cerus-promise"
+	];
+
+	var router;
+
+	self.init_ = function(cerus) {
+		router = require("./lib/router")(cerus);
+	}
+
+	self.router = function() {
+		return router;
+	}
+
+	return self;
+}
